51d5000d007e948ea707a9cb26773ed5aa597405
d2c_cyf <cyfsoftware@126.com>
2017-05-11 17:57:38 +0800

    Initial Commit

---
+++ CRConfirmProductProtocol/CRConfirmProductProtocol.h
@@ -0,0 +1,19 @@
+//
+//  CRConfirmProductProtocol.h
+//  CRConfirmProductProtocol
+//
+//  Created by d2c_cyf on 17/5/11.
+//  Copyright © 2017年 d2c_cyf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CRConfirmProductProtocol.
+FOUNDATION_EXPORT double CRConfirmProductProtocolVersionNumber;
+
+//! Project version string for CRConfirmProductProtocol.
+FOUNDATION_EXPORT const unsigned char CRConfirmProductProtocolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRConfirmProductProtocol/PublicHeader.h>
+
+
